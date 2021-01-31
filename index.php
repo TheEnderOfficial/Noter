734a86a3d9a5abdfa3caac3653f6240e255e588d
Ender <enderteamyt.official@gmail.com>
2021-01-31 18:31:31 +0300

    First

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar">
+                <a href="#" class="heading">Noter</a>
+                <a href="#" class="nav-link"><img class="header-image" src="img/account.png"></a>
+                <a href="#" class="nav-link">Заметки</a>
+            </nav>
+        </header>
+        
+        <div align="center" class="content">
+            <div align="center">
+                <span class="heading">Ваши заметки</span>
+            </div>
+            <div class="note">
+                <span class="note-header">
+                    asf
+                </span>
+                <br>
+                <span class="note-description">
+                    asf
+                </span>
+            </div>
+        </div>
+        <footer class="footer">
+            <nobr>
+            <div class="footer-left">
+                Copyright © Ender 2020-2021<br>
+                Все права защищены лицензией GPLv3
+            </div>
+            <div class="footer-right">
+                <br>
+                <a href="http://github.com/TheEnderOfficial"><img class="footer-image" src="img/github.png"></a>
+                <a href="http://vk.com/theender_yt"><img class="footer-image" src="img/vk.png"></a>
+            </div>
+            </nobr>
+        </footer>
+    </body>
+</html>
